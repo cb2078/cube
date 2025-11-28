64a240d219d7fb7afbf38a2c25e3b06316de8183
cb2078 <cb2078@york.ac.uk>
2025-11-28 12:06:54 +0000

    update howh asserts work
    - print the filename and line and expression
    - break in debugger for debug builds and unreachable for release

--- common.h
+++ common.h
@@ -10,13 +10,19 @@
 #include <threads.h>
 
 #define ABS(x) ({ typeof(x) a=(x); a>0 ? a : -a; })
-#define ASSERT(cond) ((cond) ? (void)0 : UNREACHABLE())
+#define ASSERT(cond)\
+    (!(cond) ? fprintf(stderr, "%s:%d: Assertion '%s' failed.\n", __FILE__, __LINE__, #cond), UNREACHABLE() : (void)0)
 #define ERROR(fmt, ...) (fprintf(stderr, "cube: " fmt, __VA_ARGS__), exit(1))
 #define LENGTH(x) (long long)(sizeof(x)/sizeof(x[0]))
 #define MAX(x, y) ({ typeof(x) a=(x), b=(y); a>b ? a : b; })
 #define MIN(x, y) ({ typeof(x) a=(x), b=(y); a<b ? a : b; })
 #define SWAP(x, y) do { typeof(x) z=x; x=y; y=z; } while (0)
-#define UNREACHABLE() (debugbreak(), __builtin_unreachable())
+
+#ifdef DEUBG
+#define UNREACHABLE() debugbreak()
+#else
+#define UNREACHABLE() __builtin_unreachable()
+#endif
 
 #ifdef DEBUG
 #define LOG(fmt, ...) fprintf(stderr, fmt, __VA_ARGS__)
