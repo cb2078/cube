44c6dd7d87ae3982c19cba9ced2acc77a2704dcd
cb2078 <cb2078@york.ac.uk>
2026-01-30 19:35:04 +0000

    round up the table size to the nearest multiple of TABLE_ALIGN
    making alligned alloc work

--- table.h
+++ table.h
@@ -3,7 +3,7 @@
 
 #define TABLE_ALIGN 16
 
-#define TABLE_SIZE(entries, bits) (((entries)*(bits)+7)/8)
+#define TABLE_SIZE(entries, bits) ((((entries)*(bits)+7)/8)+15)/16*16
 #define TABLE_SET_MIN(t, bits, i, x) (void)(table_get((t), (bits), (i)) > (x) ? table_set((t), (bits), (i), (x)) : 0)
 
 static unsigned *table_new(long long, int);
