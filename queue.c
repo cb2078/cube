315746e427fa9dd9039f70d1537cdc4a8d2c9078
cb2078 <cb2078@york.ac.uk>
2026-06-26 23:56:47 +0100

    only search unvisited positions during BFS of the phase2 prune table generation
    this increases the queue efficiency from symmetries from 40.9x to 44.1x

--- queue.c
+++ queue.c
@@ -8,17 +8,17 @@ static struct queue queue_new(long long capacity)
 
 static struct search_node queue_get(struct queue *q, long long i)
 {
-    ASSERT(i < q->length);
-    return q->entries[(q->start+i)%q->capacity];
+    ASSERT(i < q->start+q->length);
+    return q->entries[q->start+i];
 }
 
 static void queue_push(struct queue *q, cube_t x, int move, int depth)
 {
-    for (int i=0; i<q->length; i++)
-        if (cube_eq(x, queue_get(q, i).cube))
+    for (long long i=0; i<q->start+q->length; i++)
+        if (cube_eq(x, q->entries[i].cube))
             return;
-    ASSERT(++q->length < q->capacity);
-    q->entries[(q->start+q->length-1)%q->capacity] = (struct search_node){x, move, depth};
+    ASSERT(q->start+(++q->length) < q->capacity);
+    q->entries[q->start+q->length-1] = (struct search_node){x, move, depth};
 }
 
 static struct search_node queue_pop(struct queue *q)
